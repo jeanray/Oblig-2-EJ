9bf67797b7cc0a594136a022ca4569a1b3ce6f75
jeanray <jean.oedegaard@gmail.com>
2017-03-15 18:15:11 +0100

    Skallfil med melding

---
+++ registrer_bilde.php
@@ -0,0 +1,4 @@
+<?php
+  print("Funksjon for å registrere bilde, som så må lede til registrering av student.<br>");
+  print("<h3>Hvis alle studenter skal ha bilde, så bør man opplyse om det på regStudent!</h3>")
+?>
