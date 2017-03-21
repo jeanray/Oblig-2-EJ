507002cec0475f8bf68d615cefedef81050776d9
jeanray <jean.oedegaard@gmail.com>
2017-03-21 05:59:44 +0100

    Versjon-bump
    Hoppet opp til skoleAdmin v1.0 - på tide nå, har vært igjennom opp til
    versjon 0.5, men nå er det så enorme endringer at vi kjører på med en ny
    version, 1.X

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>skoleAdmin v0.4</title>
+  <title>skoleAdmin v1.0</title>
   <!-- CSS-filer -->
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
   <link href="css/custom.css" rel="stylesheet">
