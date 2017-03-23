a9abd8d686912a1eb2379b7da6495e34d89b36e1
jeanray <jean.oedegaard@gmail.com>
2017-03-23 07:39:19 +0100

    Lagt til utfyllende kommentarer

--- skjemaFunksjoner.php
+++ skjemaFunksjoner.php
@@ -4,40 +4,45 @@
 noe dynamisk, som en listeboks. Da kjører man i så fall den tilhørende funksjonen i <?php skjemaListKlKoder(); ?>, og fortsetter med HTMLen,
 i dette tilfellet da med </select> osv osv. */
 
-function skjemaListKlkoder() {
-  $sql = "SELECT klassekode FROM klasse;";
+$errorArray = array();
+  // For å få globalt scope på error-arrayet så må den defineres utenfor funksjonene
 
-  $sqlObjekt = $dbLink->query($sql);
+function skjemaListKlkoder() {
+  $sql = "SELECT klassekode FROM klasse;"; // Hent ut klassekodene fra klasse-tabellen
+
+  $sqlObjekt = $dbLink->query($sql);  // Utfører spørringen
 
   if ($sqlObjekt->num_rows == "0") {  // Hvis vi ikke får noen rader fra DBHS-et
       $errorArray[] = "<div class=\"alert alert-danger\" role=\"alert\">Fatal feil: Du må først registrere en klasse før du kan registrere en student!";
       return;
   }
 
-  while ($rad = $sqlObjekt->fetch_assoc()) {
+  while ($rad = $sqlObjekt->fetch_assoc()) { // Mens vi får rader fra sql skal vi printe klassekode i <option>-tagger
     print ("<option>" . $rad["klassekode"] . "</option>");
   }
 
   if ( isset($sqlObjekt) ) {  // En if-setning her fordi vi ikke dreper scriptet, men samler opp feilmeldinger i stedet.
-    mysqli_free_result($sqlObjekt);
+    mysqli_free_result($sqlObjekt); // Frigjør sql-objektet
   }
 
 } // Funksjon for å skrive ut klassekoder avsluttes her
 
 function skjemaListBildenr() {
-  $sql = "SELECT bildenr FROM bilde;";
+  $sql = "SELECT bildenr FROM bilde;";  // Hent ut bildenr fra tabellen bilde
+
+  $sqlObjekt = $dbLink->query($sql);  // Utfører spørringen
 
   if ($sqlObjekt->num_rows == "0") {  // Hvis vi ikke får noen rader fra DBHS-et
     $errorArray[] = "<div class=\"alert alert-danger\" role=\"alert\">Fatal feil: Du må først registrere et bilde!";
     return;
   }
 
-  while ($rad = $sqlObjekt->fetch_assoc()) {
+  while ($rad = $sqlObjekt->fetch_assoc()) { // Så lenge vi får bilder fra sql skal vi printe bildenr med option-tagger rundt
     print ("<option>" . $rad["bildenr"] . "</option>");
   }
 
   if ( isset($sqlObjekt) ) {  // En if-setning her fordi vi ikke dreper scriptet, men samler opp feilmeldinger i stedet.
-    mysqli_free_result($sqlObjekt);
+    mysqli_free_result($sqlObjekt); // Frigjør sql-objektet
   }
 } // Funksjon for å skrive ut bildenr ender her
 
