e102fff1d5ea2eb5026dbcc743c1aac0d58e52b7
jeanray <jean.oedegaard@gmail.com>
2017-03-24 21:25:11 +0100

    Opprettet fil og lagt til noe kommentering med ideer

---
+++ vis_bilder.php
@@ -0,0 +1,9 @@
+<?php
+print(<h3>Vis alle bilder</h3>);
+
+/* Litt usikker på hva han ønsker her. En liste over alle bilder, med tilhørende beskrivelse osv virker logisk,
+og det kunne vært litt artig å hatt det slik at når man trykket på filnavnet så poppet bildet opp, men ikke på
+en egen side som om du hadde trykket på linken til bildet (site/bilder/filnavn), men heller i form av en fancy
+jquery-boks eller bildekarusell eller noe i den duren.*/
+
+?>
