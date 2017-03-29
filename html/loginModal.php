70392eba9fbf84a650be942b3d206fb0cbd34aa9
jeanray <jean.oedegaard@gmail.com>
2017-03-29 10:44:25 +0200

    Fjernet knapp for registrering av bruker enn så lenge Også lagt til noe kommentering

--- html/loginModal.php
+++ html/loginModal.php
@@ -1,17 +1,16 @@
-<!-- Trigger the modal with a button -->
+<!-- Får finne en bedre løsning, mulig med javascript ev. at php-innloggingssjekken feiler så kommer denne opp -->
 <br>
 <button type="button" class="btn btn-info" data-toggle="modal" data-target="#loginModal">Logg inn</button>
-<button type="button" class="btn btn-info" data-toggle="modal" data-target="#regModal">Registrer bruker</button>
 
 <div id="loginModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Vennligst logg inn:</h4>
+        <h4 class="modal-title">Vennligst logg inn for å bruke skoleAdmin:</h4>
       </div>
       <div class="modal-body">
-        <form>
+        <form> <!-- Start form-tag her -->
           <div class="form-group">
             <label for="brukernavn" class="form-control-label">Brukernavn:</label>
             <input type="text" class="form-control" id="bruker" name="bruker" placeholder="Brukernavn">
@@ -25,38 +24,7 @@
             <button type="button" class="btn btn-info" data-dismiss="modal">Lukk vinduet</button>
             <button type="submit" class="btn btn-success" data-dismiss="modal">Logg inn</button>
           </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div id="regModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Registrer ny bruker:</h4>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="nyttBrukernavn" class="form-control-label">Ønsket brukernavn:</label>
-            <input type="text" class="form-control" id="bruker" name="bruker" placeholder="Brukernavn">
-          </div>
-          <div class="form-group">
-            <label for="passord" class="form-control-label">Passord:</label>
-            <input type="password" class="form-control" name="nyHemmelighet" id="nyHemmelighet" placeholder="Passord">
-          </div>
-          <div class="form-group"> <!-- Burde legge til en dobbelsjekk av passord, men må blande inn php eller js -->
-            <label for="passord" class="form-control-label">Vennligst gjenta passordet:</label>
-            <input type="password" class="form-control" name="hemmelig2" id="hemmelig2" placeholder="Passord">
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-info" data-dismiss="modal">Lukk vinduet</button>
-            <button type="submit" class="btn btn-success" data-dismiss="modal">Registrer</button>
-          </div>
-        </form>
+        </form> <!-- Avslutter form her -->
       </div>
     </div>
   </div>
