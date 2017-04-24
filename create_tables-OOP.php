560c26ea155f23d934590b8a40e0ce1d6ec57b64
jeanray <jean.oedegaard@gmail.com>
2017-04-24 08:11:18 +0200

    Endret create tables
    Nå bruker den OOP-metodene som føles mer korrekt ut, da resten av
    programmet bruker det, samtidig så er det lagt til create-spørringer
    for tabellen til lagring av brukernavn og hashet passord.

--- create_tables-OOP.php
+++ create_tables-OOP.php
@@ -1,5 +1,5 @@
 <?php
-include("dbTilkobling.php");
+include("dbTilkoblingOOP.php");
   // Inkluderer tilkoblingsinformasjon
 
 $sql = "CREATE TABLE klasse (
@@ -11,7 +11,7 @@ $sql = "CREATE TABLE klasse (
     // Vi vet at fagkode alltid er 7 tegn, derfor bruker vi char der.
     // Kolonnen fagkode blir satt til å være primærnøkkel.
 
-if (mysqli_query($dbLink, $sql)) { // Vi kjører spørringen mot databasen via $dblink
+if ($dbLink->query($sql)) { // Vi kjører spørringen mot databasen via $dblink
   print("Tabellen \"klasse\" ble opprettet uten feil.\n<br>");
 } else { // Ev. feilmelding blir skrevet ut. Vanlig feil her er at tabellen allerede eksisterer.
   print "Feil ved registrering av tabell, feilmelding fra mysql: \"" . mysqli_error($dbLink) . "\".<br>";
@@ -27,33 +27,23 @@ $sql = "CREATE TABLE student (
     // av data fra to forskjellige kolonner, fagkode og oppgavenr. I tillegg er det satt en fremmednøkkel, som
     // er en referanse til primørnøkkelen i tabellen fag, som er fagkode-kolonnen.
 
-if (mysqli_query($dbLink, $sql)) { // Vi utfører spørringen, og skriver ut ev. feilmeldinger vi måtte få.
-  print("Tabellen \"oppgave\"ble opprettet i databasen \"$database\".\n<br>");
-} else { // Ev. feilmelding blir skrevet ut. Vanlig feil her er at tabellen allerede eksisterer.
-  print "Feil ved registrering av tabell: " . mysqli_error($dbLink) . "<br>";
-}
-// Over bruker vi mysqli_multi_query for å utføre flere enn en spørring på en gang, og legger til alle oppføringene
-// i tabellen "oppgaver" ved hjelp av en enkelt mysqli-kommando.
-
-
-/* Nedenfor må tilpasses oblig2, og vi må endre til OOP i hele dokumentet
-
-
-$sql = "CREATE TABLE student (
-  brukernavn CHAR(2) PRIMARY KEY,
-  fornavn VARCHAR(32) NOT NULL,
-  etternavn VARCHAR(64) NOT NULL,
-  klassekode VARCHAR(32),
-  FOREIGN KEY (klassekode) REFERENCES klasse(klassekode)
-  );"; // SQL-kode for å opprette en tabell til, oppgave, som har tre kolonner og en primærnøkkel som består
-    // av data fra to forskjellige kolonner, fagkode og oppgavenr. I tillegg er det satt en fremmednøkkel, som
-    // er en referanse til primørnøkkelen i tabellen fag, som er fagkode-kolonnen.
-
-if (mysqli_query($dbLink, $sql)) { // Vi utfører spørringen, og skriver ut ev. feilmeldinger vi måtte få.
+if ($dbLink->query($sql)) { // Vi utfører spørringen, og skriver ut ev. feilmeldinger vi måtte få.
   print("Tabellen \"oppgave\"ble opprettet i databasen \"$database\".\n<br>");
 } else { // Ev. feilmelding blir skrevet ut. Vanlig feil her er at tabellen allerede eksisterer.
   print "Feil ved registrering av tabell: " . mysqli_error($dbLink) . "<br>";
 }
 
-mysqli_close($dbLink);
-?>
+  // Under lager vi den enkeltstående tabellen uten relasjoner, som kun inneholder brukernavn og passord.
+  // Siden vi bruker password_hash må domenet for attributten kjenneord være såpass stor for å ta høyde for både
+  // fremtidige nye krypteringsfunksjoner, samt at hashen kan bli over 60 tegn med password_hash per i dag.
+
+$sql = "CREATE TABLE brukarar (
+  brukarnamn VARCHAR(64) PRIMARY KEY,
+  kjenneord VARCHAR(255)
+);";
+
+if ($dbLink->query($sql)) { // Vi utfører spørringen, og skriver ut ev. feilmeldinger vi måtte få.
+  print("Tabellen \"brukarar\"ble opprettet i databasen \"$database\".\n<br>");
+} else { // Ev. feilmelding blir skrevet ut. Vanlig feil her er at tabellen allerede eksisterer.
+  print "Feil ved registrering av tabell: " . mysqli_error($dbLink) . "<br>";
+}
