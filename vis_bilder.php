6370097f33dc1edc4feb1fa6c95f27c598452ff1
jeanray <jean.oedegaard@gmail.com>
2017-05-07 07:23:43 +0200

    Endret navn samt små endringer
    Emil sine endringer

--- vis_bilder.php
+++ vis_bilder.php
@@ -1,5 +1,26 @@
 <?php
-print(<h3>Vis alle bilder</h3>);
+// include(".php"); -- Innloggingssjekken deres
+include("dbTilkoblingOOP.php");
+
+print("<h3>Vis alle bilder</h3>");
+
+$sqlSpørring = "SELECT * FROM bilde;";
+$sqlResultat = mysqli_query($dbLink, $sqlSpørring) or die ("Det skjedde en feil med databasetilkoblingen.");
+
+print('<table class="table table-striped">');
+print('<thead> <tr> <th>Bildenummer</th> <th>Opplastningsdato</th> <th>Filnavn</th> <th>Bildebeskrivelse</th> <th>Visning</th> </tr> </thead>');
+
+foreach ($sqlResultat as $verdi) {
+  $bildenr = $verdi["bildenr"];
+  $opplastningsdato = $verdi["opplastningsdato"];
+  $filnavn = $verdi["filnavn"];
+  $beskrivelse = $verdi["beskrivelse"];
+  $bildebane = "/bilder";
+
+  print("<tbody> <tr> <td>$bildenr</td> <td>$opplastningsdato</td> <td>$filnavn</td> <td>$beskrivelse</td> <td><a href='$bildebane/$filnavn'</td> </tr> </tbody>");
+}
+
+print('</table>');
 
 /* Litt usikker på hva han ønsker her. En liste over alle bilder, med tilhørende beskrivelse osv virker logisk,
 og det kunne vært litt artig å hatt det slik at når man trykket på filnavnet så poppet bildet opp, men ikke på
