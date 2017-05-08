fee815acbd75eb1fa422fb04c584cbafacd7c397
jeanray <jean.oedegaard@gmail.com>
2017-05-08 23:35:23 +0200

    Funksjon for å endre bildebeskrivelse

--- endre_bilder.php
+++ endre_bilder.php
@@ -1,5 +1,44 @@
 <?php
-  print("<h4>Funksjon for å endre bildebeskrivelse, nummer skal endres i endre_student</h4>");
-  print("Pulldownmeny med studentnavn+bildenr ville vært fint, ev kun bildenr.<br>");
-  print("Man får så opp en tekstform som gir mulighet for å endre beskrivelsen.<br>");
+include_once("loginFunksjoner.php");
+
+if (ikkeInnlogget()) {
+  echo '<META HTTP-EQUIV=REFRESH CONTENT="3; innlogging.php">';
+  die("<div class=\"alert alert-danger\">Du må være logget inn for å bruke denne siden, <a href=\"innlogging.php\">vennligst trykk her om du ikke blir videresendt.</a></div>");
+}
+
+print("<h3>Endre bilde</h3>");
+print('<form method="POST" action="" id="endreBildeSkjema" name="endreBildeSkjema">');
+print("Velg bilde du vil endre: <br>");
+
+print('<select id="skjemaVelgBildenr" name="skjemaVelgBildenr">');
+
+$sql = "SELECT * FROM bilde;";
+
+$sqlObjekt = $dbLink->query($sql);
+
+while ($svar = $sqlObjekt->fetch_assoc()) {
+  print ("<option>" . $svar["bildenr"] . "</option>");
+}
+print("</select><br><br>");
+print('<input type="submit" name="skjemaEndreBilSub" id="skjemaEndreBilSub" class="btn btn-primary" value="Endre valgt bilde">');
+print("</form>");
+
+$sqlObjekt->free_result();
+
+if (isset($_POST["skjemaEndreBilSub"])) {
+
+  $valgtBildeFraSkjema = $_POST["skjemaVelgBildenr"];
+
+  $sql = "SELECT beskrivelse FROM bilde WHERE bildenr='$valgtBildeFraSkjema';";
+
+  $sqlObjekt = $dbLink->query($sql);
+
+  $svar = $sqlObjekt->fetch_assoc();
+    // Vet vi bare får en rad tilbake pga regel for primary key i SQL
+  print('<form method="POST" id="skjemaEndreBeskriv" name="skjemaEndreBeskriv" action="">');
+  print('<h4>Endre beskrivelse på bildet under:</h4>');
+  print("<input type=\"text\" value=\"" . $svar["beskrivelse"] . "\" id=\"skjemaNyBeskriv\" name=\"skjemaNyBeskriv\">");
+  print("</form>");
+}
+
 ?>
